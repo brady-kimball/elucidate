40f3cd6abbefc7bdb8d4ee6c1e1751ed9936f427
brady-kimball <kimball.brady@gmail.com>
2017-05-16 09:25:39 -0700

    Populate User Controller (show, create, update)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,30 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    render :show
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render :show
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      render :show
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :avatar, :password)
+  end
 end
