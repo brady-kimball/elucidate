b621229fb056399905c2feed536e3760888874c2
brady-kimball <kimball.brady@gmail.com>
2017-05-16 10:24:01 -0700

    Add react-router-dom, initialize entry file, root and app

---
+++ frontend/components/app.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+
+const App = (props) => (
+  <h1> React is working </h1>
+);
+
+
+export default App;
---
+++ frontend/elucidate.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Root from './components/root';
+
+document.addEventListener('DOMContentLoaded', () => {
+  let store;
+  const root = document.getElementById("root");
+
+
+
+  ReactDOM.render(<Root />, root);
+});
